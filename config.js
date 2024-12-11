97ae2ce99d58c9579afdd31b5ede63c28dfdf692
setdoso <137121456+setdoso@users.noreply.github.com>
2024-12-11 13:33:07 +0100

    Update config.js

--- config.js
+++ config.js
@@ -29,8 +29,8 @@ module.exports = {
     groupLink: process.env.GcLink || "https://chat.whatsapp.com/E490r0wSpSr89XkCWeGtnX",
     warns: Number(process.env.Warn_Limits) || 3,
     cooldown: Number(process.env.Cooldown_Timer_Seconds) || 5, // default cooldown time per command in seconds
-    mongoUrl: process.env.MongoDB || "YOUR_MONGODB_URL",
-    sessionId: process.env.SESSION_ID || "YOUR_SESSION_ID",
+    mongoUrl: process.env.MongoDB || "mongodb+srv://sam:sam@cluster0.u1smxsv.mongodb.net/?retryWrites=true&w=majority",
+    sessionId: process.env.SESSION_ID || "jiG25LGHMW_A_N_Y_A_V_2_K1Mpg2pkEq",
 
     /**
      * thumbnails and medias
